c26f3b00f0b81da82790986fdc705a931efef6be
EleetCodeR <vramane007@gmail.com>
2024-12-08 19:18:32 +0530

    Compile time constants in java

---
+++ main/src/CompileTimeConstant.java
@@ -0,0 +1,29 @@
+public class CompileTimeConstant {
+    static  int staticVariable;
+    int instanceVariable;
+    public static void main(String[] args) {
+        int x;
+        int a, b, c;
+        String str1, str2;
+        Object obj = null;
+
+        int m = 20;
+        int n = m = 10;
+
+        int p, q = 10, r;
+
+        System.out.println("x: " + x); // CTE variable uses without initialization.
+
+        System.out.println("instanceVariable: " + new CompileTimeConstant().instanceVariable);
+        System.out.println("staticVariable: " + staticVariable);
+
+
+        x = 10;
+        if (x == 10) { // Compiler has no way of knowing that this condition is always TRUE.
+            a = 10;
+            System.out.println("a: " + a); // VALID.
+        }
+        System.out.println("a: " + a); // CTE
+        // (Compiler identifies an execution path where the variable is accessed without being initialized).
+    }
+}
